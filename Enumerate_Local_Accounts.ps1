3e4b9961eb829adf1652920bd13bfa10f3da1a7f
PeteSampras <PeteSampras.mmobugs@gmail.com>
2025-11-06 10:51:41 -0800

    Create Enumerate_Local_Accounts.ps1

---
+++ Enumerate_Local_Accounts.ps1
@@ -0,0 +1,38 @@
+# this script assumes you have a list of IPs with a column header of ip.
+# the purpose of this output is to provide additional information beyond an IP for your network so you can make more informed queries later
+
+#date and time stamps
+$Date = Get-Date -Format "yyyyMMdd"
+$Time = Get-Date -Format "hhMMss"
+
+#comment $creds out if you are using in multiple scripts or it will be super annoying
+$creds = Get-Credential
+
+#change csv files to match what you need
+$inputfile = ".\AllHosts.csv"
+$outputfile = ".\"+$Date+"_"+$Time+"_Local_Users.csv"
+
+$targets = Import-Csv $inputfile | select -ExpandProperty ip | Sort-Object -Unique
+
+#build out initial computer info
+Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {
+#enumerate local users to check if Admin
+
+$localusers = Get-CimInstance -ClassName Win32_UserAccount
+foreach ($user in $localusers) {
+    #Get-LocalGroupMember -Member $user.Name
+    $isadmin = Get-LocalGroupMember -Group 'Administrators' | where {$_.Name -eq $user.Caption}
+    $privilege = "User"
+    if ($isadmin.Length -eq 1) {
+        $privilege = "Admin"
+    }
+    [PSCustomObject]@{
+        Domain = (Get-WmiObject Win32_ComputerSystem).Domain
+        HostName = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+        LocalUser = $user.Caption
+        Privilege = $privilege
+        Date = $Date
+        Time = $Time
+    }
+}
+} | Export-Csv -Path $outputfile
