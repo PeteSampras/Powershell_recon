edbd4573a1f6239f677545a00a80f7db953fcc5c
PeteSampras <PeteSampras.mmobugs@gmail.com>
2025-11-06 10:40:24 -0800

    Update 00_Build_Host_List.ps1

--- 00_Build_Host_List.ps1
+++ 00_Build_Host_List.ps1
@@ -1,12 +1,19 @@
 # this script assumes you have a list of IPs with a column header of ip.
 # the purpose of this output is to provide additional information beyond an IP for your network so you can make more informed queries later
 
+#date and time stamps
+$Date = Get-Date -Format "yyyyMMdd"
+$Time = Get-Date -Format "hhMMss.sss"
+
 #comment $creds out if you are using in multiple scripts or it will be super annoying
 $creds = Get-Credential
+
 #change csv files to match what you need
 $inputfile = ".\hosts.csv"
 $outputfile = ".\AllHosts.csv"
+
 $targets = Import-Csv $inputfile | select -ExpandProperty ip | Sort-Object -Unique
+
 #build out initial computer info
 Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {
 $adapters = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object Name, Status, MacAddress, LinkSpeed
